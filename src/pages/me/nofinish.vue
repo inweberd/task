6ab437fd38bfb0e4bf60e2284e79498bd8b30ce0
董继彬 <dongjibin8@163.com>
2024-07-06 17:09:36 +0800

    feat: 我的点击详情页面展示图片

--- src/pages/me/nofinish.vue
+++ src/pages/me/nofinish.vue
@@ -1,10 +1,19 @@
 <template>
-    <div>
-        <van-nav-bar title="开发中" safe-area-inset-top fixed placeholder @click-left="router.back()" left-text="返回" left-arrow></van-nav-bar>
-        <van-image :src="shareholder" width="94%" height="100%" style='margin-left:3%' fit="cover"></van-image>
-    </div>
+  <div>
+    <van-nav-bar
+      title="详情"
+      safe-area-inset-top
+      fixed
+      placeholder
+      @click-left="$router.back()"
+      left-text="返回"
+      left-arrow
+    ></van-nav-bar>
+    <van-image :src="my" width="94%" height="100%" style="margin-left: 3%" fit="cover"></van-image>
+  </div>
 </template>
 
-<script lang='ts' setup>
+<script lang="ts" setup>
 import shareholder from '@/assets/img/kaifa.png'
+import my from '@/assets/img/my.jpg'
 </script>

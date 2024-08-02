aafb9b8539b0f61ccca0e5b053e7c5896e0097b4
董继彬 <dongjibin8@163.com>
2024-08-02 19:21:37 +0800

    feat: 修改分享链接和下载链接域名

--- src/pages/me/mine.vue
+++ src/pages/me/mine.vue
@@ -69,7 +69,7 @@ function goDownload() {
 
     // const { VITE_APP_URL } = import.meta.env
     // 跳转下载
-    window.location.href = `https://tc.ijylmwy.com/download`
+    window.location.href = `http://ssn.rgrma.cn/download`
   } catch (e) {
     _notice('下载失败')
     // state.loading.app = false

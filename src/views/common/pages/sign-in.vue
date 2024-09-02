ae460a80e0798a47b63229cd74c2c5a623ce84f1
dongjibin <dongjibin8@163.com>
2024-09-02 22:14:34 +0800

    feat: 增加验证码

--- src/views/common/pages/sign-in.vue
+++ src/views/common/pages/sign-in.vue
@@ -87,6 +87,18 @@
                 size="large"
               ></el-input>
             </el-form-item>
+            <el-form-item>
+              <template #label>
+                <span class="text-white">验证码</span>
+              </template>
+              <div class="d-flex w-100">
+                <el-input v-model="state.struct.code" size="large"></el-input>
+                <canvas ref="verifyRef" @click="draw" :width="150" height="50"></canvas>
+                <!--<el-button @click="getCode" size="large" class="ms-2" color="#409EFF">-->
+                <!--  <span style="color: #fff">{{ viewCode }}</span>-->
+                <!--</el-button>-->
+              </div>
+            </el-form-item>
           </el-form>
 
           <el-button
@@ -125,7 +137,7 @@ import logo from '@/views/common/assets/logo.png'
 
 import { storeToRefs } from 'pinia'
 import { POST } from '@/utils/axios'
-import { onMounted, reactive, watch } from 'vue'
+import { onMounted, reactive, ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
 import { useUsers } from '@/store/users'
@@ -134,6 +146,7 @@ import { showToast } from 'vant'
 const { info, token, status } = storeToRefs(useUsers())
 
 const router = useRouter()
+const viewCode = ref('')
 const state = reactive({
   struct: {
     code: '',
@@ -158,15 +171,100 @@ const state = reactive({
   }
 })
 
+const verifyRef = ref(null)
+const codeState = reactive({
+  pool: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
+  // 声明两个变量存验证码的宽高
+  width: 150,
+  height: 50,
+  // 验证码最终存储的位置
+  imgCode: ''
+})
+const getCode = () => {
+  viewCode.value = ''
+  let codeString = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let codeArray = codeString.split('')
+  let num = codeArray.length
+  let newCodeArray = []
+  for (let i = 0; i < 5; i++) {
+    let index = Math.floor(Math.random() * num)
+    newCodeArray.push(codeArray[index])
+  }
+  viewCode.value = newCodeArray.join('')
+}
+
+const randomNum = (min, max) => {
+  // parseInt(string, radix) 解析一个字符串并返回指定基数的十进制整数，radix 是 2-36 之间的整数，表示被解析字符串的基数
+  return parseInt(Math.random() * (max - min) + min, 10)
+}
+// 随机生成颜色
+const randomColor = (min, max) => {
+  const r = randomNum(min, max)
+  const g = randomNum(min, max)
+  const b = randomNum(min, max)
+  return `rgb(${r},${g},${b})`
+}
+// 实现验证码的绘制
+const draw = () => {
+  codeState.imgCode = ''
+  // getCode()
+  // ① 获取canvas属性
+  const ctx = verifyRef.value.getContext('2d')
+  // ② 填充背景颜色
+  ctx.fillStyle = randomColor(20, 256)
+  ctx.fillRect(0, 0, 150, 50) // ctx.fillRect(矩形起始点的x坐标，矩形起始点的y坐标,矩形的宽度，矩形的高度) fillRect()方法绘制一个填充了内容的矩形
+  // fillText()  指定的坐标上绘制文本字符串，并使用当前的 fillStyle 对其进行填充
+  // ③ 生成随机数，随机数旋转
+  for (let i = 0; i < 5; i++) {
+    const text = codeState.pool[randomNum(0, codeState.pool.length)]
+    codeState.imgCode += text
+    console.log('state.imgCode', codeState.imgCode)
+    const fontSize = randomNum(16, 30)
+    ctx.font = fontSize + 'px Simhei'
+    ctx.fillStyle = randomColor(0, 150) // 设置文字颜色
+    ctx.textBaseline = 'top' // 述绘制文本时，当前文本基线的属性
+    const deg = randomNum(-15, 15) // 旋转的角度
+    ctx.save() // 保存当前的绘图状态
+    ctx.translate(30 * i + 15, randomNum(10, 25)) // ctx.translate(x, y); x 点的水平方向、原始的 y 点垂直方向进行平移变换
+    ctx.rotate((deg * Math.PI) / 180) // Math.PI   3.14  【随机生成旋转角度】
+    ctx.fillText(text, 0, 0)
+    ctx.restore() // ctx.restore() 方法后，会将绘图上下文恢复到最近一次保存的状态，也就是撤销之前的所有变换和样式设置，使得后续的绘制不受影响
+    // 使用 save() 方法保存默认的状态，使用 restore() 进行恢复
+  }
+  // ④ 生成40个点和4条线形成干扰防止OCR
+  for (let i = 0; i < 5; i++) {
+    ctx.beginPath()
+    ctx.strokeStyle = randomColor(30, 256)
+    ctx.moveTo(randomNum(0, codeState.width), randomNum(0, codeState.height))
+    ctx.lineTo(randomNum(0, codeState.width), randomNum(0, codeState.height))
+    ctx.fillStyle = randomColor(0, 150)
+    ctx.stroke() // 绘制线段
+  }
+  for (let x = 0; x < 40; x++) {
+    ctx.beginPath()
+    // ctx.arc( ①圆弧中心（圆心）的 x 轴坐标 ②圆弧中心（圆心）的 y 轴坐标 ③圆弧的半径 圆弧的起始点 ④x 轴方向开始计算，单位以弧度表示  ⑤圆弧的终点，单位以弧度表示)
+    ctx.arc(randomNum(0, codeState.width), randomNum(0, codeState.height), 1, 0, 2 * Math.PI)
+    ctx.closePath()
+    // 笔点返回到当前子路径起始点的方法。它尝试从当前点到起始点绘制一条直线。如果图形已经是封闭的或者只有一个点，那么此方法不会做任何操作
+    ctx.stroke()
+    ctx.fillStyle = randomColor(150, 200)
+    ctx.fill()
+  }
+  return codeState.imgCode
+}
 const SignIn = async () => {
   if (!state.struct.account) return showFailToast('请输入账号')
   if (!state.struct.password) return showFailToast('请输入密码')
-
+  if (!state.struct.code) return showFailToast('请输入验证码')
+  if (state.struct.code.toLowerCase() !== codeState.imgCode.toLowerCase()) {
+    return showFailToast('验证码错误！')
+  }
   state.status.wait = true
 
   const params = {
     account: state.struct.account,
-    password: state.struct.password
+    password: state.struct.password,
+    code: codeState.imgCode
   }
 
   const { code, data, msg } = await POST(`/api/comm/login`, params)
@@ -192,6 +290,8 @@ const SignIn = async () => {
   router.push({ path: '/' })
 }
 onMounted(() => {
+  getCode()
+  draw()
   window.localStorage.removeItem('userInfo')
   window.localStorage.removeItem('token')
 })
